cf431ecf027861546a078bebef41b26efd37f870
Laura Nunez <lauranunez@Laura-Nunezs-MacBook-Pro.local>
2016-01-26 21:01:36 -0600

    update 126900

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ var mynumber = Math.floor((Math.random() *50) + 1);
 var yournumber = prompt("What's your number");
 if(yournumber <= mynumber)
 {
-    console.log("Yatzee!");
+    alert("Yatzee!");
 }
 else
 {
-    console.log("Womp Womp");
+    alert("Womp Womp");
 }
