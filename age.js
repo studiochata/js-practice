8d0350098fc7f7d35789545bc5d8eddb3ce4dfe6
Laura Nunez <lauranunez@Laura-Nunezs-MacBook-Pro.local>
2016-02-02 21:00:58 -0600

    age

---
+++ age.js
@@ -0,0 +1,8 @@
+var dob='19800810';
+var year=Number(dob.substr(0,4));
+var month=Number(dob.substr(4,2))-1;
+var day=Number(dob.substr(6,2));
+var today=new Date();
+var age=today.getFullYear()-year;
+if(today.getMonth()<month || (today.getMonth()==month && today.getDate()<day)){age--;}
+alert(age);
