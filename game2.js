f231f65d8844618406e78faabaa85270ac5723e5
Laura Nunez <lauranunez@Laura-Nunezs-MacBook-Pro.local>
2016-02-02 20:53:32 -0600

    update

--- game2.js
+++ game2.js
@@ -1,7 +1,12 @@
-var mynumber = Math.floor((Math.random() *50) + 1);
+//var mynumber = Math.floor((Math.random() *50) + 1); 
 
+var mynumber = 12;
 var yournumber = prompt("What's your number?");
-while (yournumber >= mynumber)
-{
-    console.log("Yatzee!");
-}
+
+while (yournumber != mynumber) {
+
+    console.log("Womp Womp");
+  var yournumber = prompt("Please try again");
+ 
+ } 
+    console.log ("Yatzee");
